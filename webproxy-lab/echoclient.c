3deb7f365ffc9a79c8505f8381647afbb5393590
Jiyun Lee <kuri12071112@gmail.com>
2025-07-04 18:09:10 +0900

    Add: echoclient + echoserveri

---
+++ webproxy-lab/echoclient.c
@@ -0,0 +1,32 @@
+#include "csapp.h"
+
+
+int main (int argc, char **argv) 
+{
+    int clientfd;    /* Initializing a buffer */
+    char host, *port, buf[MAXLINE]; 
+    rio_t rio;
+
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+        exit(0);
+    }
+    host = argv[1];
+    port = argv[2];
+
+    clientfd = Open_clientfd(host,port);
+    Rio_readinitb(&rio, clientfd);
+
+    /* From client's perspective echoing is just read in what I typed */
+    /* reading what has been typed on the standard input, and as long as thats not NULL*/
+    while (Fgets(buf, MAXLINE, stdin) != NULL) {  
+        /* write it to the client file descriptor (sending it over to the network)*/
+        Rio_writen(clientfd,buf, strlen(buf)); 
+        /*gettinng back from the server, read from the file descriptor, fill the result back in the buffer */
+        Rio_readlineb( &rio, buf, MAXLINE); 
+        /* prints ( new line is included )*/
+        Fputs(buf, stdout);
+    }
+    Close(clientfd);
+    exit(0);
+}
---
+++ webproxy-lab/echoserveri.c
@@ -0,0 +1,26 @@
+#include "csapp.h"
+void echo(int connfd);
+
+
+int main(int argc, char **argv)
+{
+    int listenfd, connfd;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr; /* Enough room for any addr.
+    It's part of the gneeric API, for an api that is big enough to store name of the host */
+    char client_hostname[MAXLINE], client_port[MAXLINE];
+
+    listenfd = Open_listenfd(argv[1]);
+    while (1) {
+        clientlen = sizeof(struct sockaddr_storage); /* Important! */
+        /* info about the client : IP Address*/
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        /* pass in client address, andd it comes back with the domain name of the client*/
+        Getnameinfo((SA *) &clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0); 
+        /* client_hostname client_port are both buffers (passing their length here MAXLINE) filling those in with the string representations of the hostname & the port */
+        printf("Connected to (%s, %s)\n", client_hostname, client_port);
+        echo(connfd);
+        Close(connfd);
+    }
+    exit(0);
+}
