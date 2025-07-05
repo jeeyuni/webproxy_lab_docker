99cf32d268bec527fe25a3006d2030cae995020b
Jiyun Lee <kuri12071112@gmail.com>
2025-07-05 10:28:35 +0900

    Add: serve_dynamic

--- webproxy-lab/tiny/tiny.c
+++ webproxy-lab/tiny/tiny.c
@@ -52,7 +52,25 @@ void serve_static(int fd, char *filename, int filesize) {
   /* length here(idk where uhh   ) is the lengh of the file ( doesn't include the bytes in the header )*/
 }
 
+void serve_dynamic(int fd, char *filename, char *cgiargs) 
+{
+  char buf[MAXLINE], *emptylist[] = {NULL};
 
+  /* Return first par tof HTTP respnse */
+  sprintf(buf, "HTTP/1.0 200 OK\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Server: Tiny Web Server\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+
+  if (Fork() == 0) { /* Child */
+    /* Real server would set all CGI vars here */
+    setenv("QUERY_STRING", cgiargs, 1);     /* environment variable */
+    DUp2(fd, STDOUT_FILENO);                /* Redirect stdout to client */
+    Execve(filename, emptylist, environ);   /* Run CGI program */
+  }
+  Wait(NULL); /* Parent waits for and reaps child */
+
+}
 
 int main(int argc, char **argv)
 {
