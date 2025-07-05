4b403e2d8e171b8241b68f3539ac405b875d992a
Jiyun Lee <kuri12071112@gmail.com>
2025-07-05 11:31:18 +0900

    Add: doit

--- webproxy-lab/tiny/tiny.c
+++ webproxy-lab/tiny/tiny.c
@@ -18,26 +18,27 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
                  char *longmsg);
 
 /* Helper function of the code */
-void serve_static(int fd, char *filename, int filesize) {
+void serve_static(int fd, char *filename, int filesize)
+{
   int srcfd;
   char *srcp, filetype[MAXLINE], buf[MAXBUF];
 
-  /* Send response headers to client 
+  /* Send response headers to client
   receives a request with a particular file name from the uri
   it looks up OS call on how big the file is (how many bytes)
   given a file descriptor for the connection.
-  This function supports not just text but also GIFS JPEG and 
+  This function supports not just text but also GIFS JPEG and
   */
-  get_filetype(filename,filetype); // which type of file is it? (GIF, JPEG ... )
+  get_filetype(filename, filetype); // which type of file is it? (GIF, JPEG ... )
   /* formatting a header information */
   /*writes the header information out to the client */
   sprintf(buf, "HTTP/1.0 200 OK\r\n"); // every line has to finish with character return and a new line
   sprintf(buf, "%sServer: Tiny Web Server\r\n"), buf;
   sprintf(buf, "%sConnection: close\r\n", buf);
-  sprintf(buf, "%sContent-length: %d\r\n", buf, filesize); //IMPORTANT
-  sprintf(buf, "%sContent-type: %s\r\n\r\n", buf,filetype); //IMPORTANT
+  sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);   // IMPORTANT
+  sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype); // IMPORTANT
   /* Sending back to the client */
-  Rio_writen(fd, buf,strlen(buf));
+  Rio_writen(fd, buf, strlen(buf));
 
   /* Send response body to client. These can be done with regular read and writes*/
   /* Get the file, open it up */
@@ -52,7 +53,7 @@ void serve_static(int fd, char *filename, int filesize) {
   /* length here(idk where uhh   ) is the lengh of the file ( doesn't include the bytes in the header )*/
 }
 
-void serve_dynamic(int fd, char *filename, char *cgiargs) 
+void serve_dynamic(int fd, char *filename, char *cgiargs)
 {
   char buf[MAXLINE], *emptylist[] = {NULL};
 
@@ -62,14 +63,58 @@ void serve_dynamic(int fd, char *filename, char *cgiargs)
   sprintf(buf, "Server: Tiny Web Server\r\n");
   Rio_writen(fd, buf, strlen(buf));
 
-  if (Fork() == 0) { /* Child */
+  if (Fork() == 0)
+  { /* Child */
     /* Real server would set all CGI vars here */
-    setenv("QUERY_STRING", cgiargs, 1);     /* environment variable */
-    DUp2(fd, STDOUT_FILENO);                /* Redirect stdout to client */
-    Execve(filename, emptylist, environ);   /* Run CGI program */
+    setenv("QUERY_STRING", cgiargs, 1);   /* environment variable */
+    DUp2(fd, STDOUT_FILENO);              /* Redirect stdout to client */
+    Execve(filename, emptylist, environ); /* Run CGI program */
   }
   Wait(NULL); /* Parent waits for and reaps child */
+}
 
+void doit(int fd)
+{
+  int is_static;
+  struct stat sbuf;
+  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+  char filename[MAXLINE], cgiargs[MAXLINE];
+  rio_t rio;
+
+  /* Read request line and headers */
+  Rio_readinitb(&rio, fd);
+  Rio_readlineb(&rio, buf, MAXLINE);
+  printf("Request headers: \n");
+  printf("%s", buf);
+  sscanf(buf, "%s %s %s", method, uri, version);
+  if (strcasecmp(method, "GET"))
+  {
+    clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
+    return;
+  }
+  read_requesthdrs(&rio);
+
+  /* Parse URI from GET request */
+  is_static = parse_uri(uri, filename, cgiargs);
+  if (stat(filename, &sbuf) < 0)
+  {
+    clienterror(fd, filename, "404", "Not found", "Tiny couldn't find this file");
+    return;
+  }
+
+  if (is_static) { /* Serve static content*/
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
+      clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
+      return;
+    }
+  }
+  else { /* Serve dynamic content */
+      if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) {
+          clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program"); 
+          return;
+      }
+      serve_dynamic(fd, filename, cgiargs);
+  }
 }
 
 int main(int argc, char **argv)
