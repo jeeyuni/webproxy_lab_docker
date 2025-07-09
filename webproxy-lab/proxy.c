d8958c435eefa13e96a3d0442a4ebd8550c89e78
Jiyun Lee <kuri12071112@gmail.com>
2025-07-09 14:07:28 +0900

    Add: updated main in proxy server

--- webproxy-lab/proxy.c
+++ webproxy-lab/proxy.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <csapp.h>
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
@@ -9,8 +10,28 @@ static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
-int main()
+int main(int argc, char **argv)
 {
-  printf("%s", user_agent_hdr);
-  return 0;
+  int listenfd, connfd;
+  char hostname[MAXLINE], port[MAXLINE];
+  socklen_t clientlen;
+  struct sockaddr_storage clientaddr;
+
+  /* Check command line args */
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  listenfd = Open_listenfd(argv[1]);
+  while (1)
+  {
+    clientlen = sizeof(clientaddr);
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+    printf("Accepted connection from (%s, %s)\n", hostname, port);
+    proxy_doit(connfd);
+    Close(connfd);
+  }
 }
